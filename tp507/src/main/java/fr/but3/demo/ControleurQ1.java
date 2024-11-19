c306b6a1694ec1bfdfba689b7bec35e7f7bf11e7
linventif <linventif@gmail.com>
2024-11-19 10:32:22 +0100

    Exo:3

--- tp507/src/main/java/fr/but3/demo/ControleurQ1.java
+++ tp507/src/main/java/fr/but3/demo/ControleurQ1.java
@@ -1,14 +1,21 @@
 package fr.but3.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 class ControleurQ1 {
   @RequestMapping({ "/hello", "/" })
   // @ResponseBody
-  public String hello() {
+  String hello(HttpServletRequest request, ModelMap modelmap) {
+    String name = request.getParameter("name");
+    if (name == null || name.isEmpty())
+      name = "World";
+    modelmap.addAttribute("cle", name);
     return "mavue";
   }
 }
